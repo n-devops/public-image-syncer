4fbc7c9b4c7d48711363f911412de3c6cd5c615b
丁鑫 <840696824@qq.com>
2024-06-17 18:35:27 +0800

    将github配置和私服配置一体化存储

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # public-image-syncer
+
 公共镜像同步器, 同步公共镜像
+
+## 同步器
+image-syncer-v1.5.4
+https://github.com/AliyunContainerService/image-syncer
+
+## 同步过程
+
+1. 利用GitHub Action同步镜像至阿里云北京站
+2. 利用Teamcity同步镜像至私服, 注意: 使用main.py 生成配置文件 images.yaml -> generate_image.yml
+
+## 项目结构
+
+* [github_auth.yaml](github_auth.yaml): GitHub 公共镜像认证配置
+* [github_image.yaml](github_image.yaml): GitHub 公共镜像拉取配置
+* [auth.yaml](images.yaml): 正在使用的私服认证配置
+* [generate_image.yml](generate_image.yml): 根据 GitHub 生成的私服镜像拉取配置
+* [images.yaml](images.yaml): 正在使用的私服拉取配置
+* [main.py](main.py): 使用python 进行配置生成
+
+## 镜像库映射规则描述
+
+1. docker.io镜像, 官方镜像放在library项目下, 非官方镜像放在相应的项目下
+    1. 官方镜像: 如 docker.io/nginx:1.17.4 -> docker-hosted.nstl-dev.com/library/nginx:1.17.4
+    2. 三方镜像: 如 docker.io/adoptopenjdk/openjdk11:latest -> docker-hosted.nstl-dev.com/adoptopenjdk/openjdk11:latest
+2. ghcr.io镜像, 映射至仓库ghcr.io项目下, 如: ghcr.io/kube-vip/kube-vip:v0.5.0 ->
+   docker-hosted.nstl-dev.com/ghcr.io/kube-vip/kube-vip:v0.5.0
+3. k8s 镜像特殊处理:
+    1. 映射至私有仓库google_containers项目下, 如: registry.k8s.io/kube-proxy:v1.28.0 ->
+       docker-hosted.nstl-dev.com/google_containers/kube-proxy:v1.28.0
+    2. coredns特殊处理, coredns镜像/coredns/coredns路径变为/coredns打平映射, 如: registry.k8s.io/coredns/coredns:
+       v1.8.6 -> docker-hosted.nstl-dev.com/google_containers/coredns:v1.8.6
+    3. kubeadm 使用时 --image-repository docker-hosted.nstl-dev.com/google_containers
---
+++ main.py
@@ -0,0 +1,56 @@
+import yaml
+
+
+def process(tempLines):
+    tempStr = '\n'.join(tempLines)
+    ymlData = yaml.safe_load(tempStr)
+    writeLines = []
+    for k, v in ymlData.items():
+        split = k.split(":", 1)
+        originalImageAddress = split[0]
+        if len(split) != 2:
+            raise ValueError("split长度不为2")
+        for vv in v:
+            writeLines.append(f"{vv}:{split[1]}:")
+            if originalImageAddress.startswith("docker.io/"):
+                # 如果是docker.io/xxx/xxx 格式
+                image_address_split = originalImageAddress.split("/")
+                # 如果长度为2, 代表是官方的镜像, 在中间增加 library
+                if len(image_address_split) == 2:
+                    originalImageAddress = f"library/{image_address_split[1]}"
+                else:
+                    # 删除 docker.io/
+                    originalImageAddress = originalImageAddress.replace("docker.io/", "", 1)
+            if originalImageAddress.startswith("registry.k8s.io/"):
+                originalImageAddress = originalImageAddress.replace("registry.k8s.io/", "google_containers/", 1)
+                # originalImageAddress如果包含/coredns/coredns, 则替换成/coredns
+                if "/coredns/coredns" in originalImageAddress:
+                    originalImageAddress = originalImageAddress.replace("/coredns/coredns", "/coredns", 1)
+            writeLines.append(f"  - docker-hosted.nstl-dev.com/{originalImageAddress}")
+    return writeLines
+
+
+if __name__ == '__main__':
+    with open("images.yaml.yaml", 'r') as file:
+        content = file.read().splitlines()
+
+    writeLines = []
+    tempLines = []
+    for line in content:
+        line = line.strip()
+        if line and not line.startswith("#"):
+            tempLines.append(line)
+        else:
+            if tempLines:
+                i = process(tempLines)
+                writeLines.extend(i)
+            writeLines.append(line)
+            tempLines = []
+
+    if tempLines:
+        i = process(tempLines)
+        writeLines.extend(i)
+
+    # 覆盖generate_image.yml
+    with open("generate_image.yml", 'w') as file:
+        file.write('\n'.join(writeLines))
