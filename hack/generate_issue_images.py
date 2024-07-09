dfa6aac00de017db80e3d55b0c8dc516d5a6fe6b
丁鑫 <840696824@qq.com>
2024-07-09 11:51:10 +0800

    修改项目逻辑

--- hack/generate_issue_images.py
+++ hack/generate_issue_images.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     mapping = mappings[repository]
     transferImage = mapping['transfer'] + ":" + tag
     transferImagesYaml = '"' + image + '":\n  - ' + mapping['transfer']
-    privateImagesYaml = '"' + transferImage + '":\n  - ' + mapping['private']
+    privateImagesYaml = f'# {image}\n"{transferImage}":\n  - {mapping['private']}'
 
     # 覆盖issue_images.yml
     with open("issue_images.yaml", 'w') as file:
